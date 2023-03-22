4e34faa67de91106abba064269b902b536881665
S.Sharma <P@ssw0rd1!>
2023-03-22 15:55:03 +0100

    Boundary value analysis for the API

--- test/justwatch_demo.test.ts
+++ test/justwatch_demo.test.ts
@@ -1,5 +1,6 @@
 import {chromium} from "playwright"
 import '@testing-library/jest-dom/extend-expect'
+import {expect,test} from "@playwright/test"
 
 describe('Launch Browser',()=>{
     // incase we have a website for this service we should show an error on the UI
@@ -29,6 +30,43 @@ describe('Launch Browser',()=>{
         // verify that an error message is thrown.
         await expect(page.getByText('congratulations the quotation has been submitted.', { exact: true })).toBeVisible();
         await browser.close();
+
+    })
+
+    // incase some people are accessing our service through a entry point
+    test("verify the quote amount in the payload",async({request,baseURL})=>{
+        let response= await request.post('${baseURL}',{
+            data:{
+                "description": "Quotation from a big company",
+                "Quote amount": "100000"
+            } });
+        // taking the response code as 400 as it would be an error scenario and entry point should return error we can expect and error code here
+        expect(response.status()).toBe(400);
+        let res= await response.json();
+        console.log(res);
+
+
+        response= await request.post('${baseURL}',{
+            data:{
+                "description": "Quotation from a big company",
+                "Quote amount": "100001"
+            } });
+        // taking the response code as 400 as it would be an error scenario and entry point should return error we can expect and error code here
+        expect(response.status()).toBe(400);
+         res= await response.json();
+        console.log(res);
+
+
+        response= await request.post('${baseURL}',{
+            data:{
+                "description": "Quotation from a big company",
+                "Quote amount": "99999"
+            } });
+        // taking the response code as 200 as it would be a positive scenario the json can then be validated for what response we receive from our service
+        expect(response.status()).toBe(200);
+         res= await response.json();
+        console.log(res);
+
 
     })
 
