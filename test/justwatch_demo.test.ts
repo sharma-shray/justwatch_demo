2ba49e86616c3080fe5d5cad4d05f6b6eccbfd47
S.Sharma <P@ssw0rd1!>
2023-03-22 15:41:37 +0100

    website test with comments

--- test/justwatch_demo.test.ts
+++ test/justwatch_demo.test.ts
@@ -4,12 +4,17 @@ import '@testing-library/jest-dom/extend-expect'
 describe('Launch Browser',()=>{
     // incase we have a website for this service we should show an error on the UI
     test('open',async()=>{
+        //create a browser
         const browser= await chromium.launch({headless: false});
         const context=await browser.newContext();
+        // create a tab
         const page= await context.newPage();
+        //navigate to the webpage
         await page.goto('http://www.jw-business-api.com');
+        // enter an invalid quotation
         await page.fill("input[name=quote amount]","1000000000");
         await page.getByRole('button', { name: 'submit' }).click();
+        // verify that an error message is thrown.
         await expect(page.getByText('Please contact our support to raise your maximum quote budget', { exact: true })).toBeVisible();
         await browser.close();
 
